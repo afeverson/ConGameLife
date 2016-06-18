248f0e552160b17c8aa7855d731082b2316b93d8
afeverson <augustfeverson@gmail.com>
2016-06-18 09:37:57 -0400

    Added boards i want to use

--- master.R
+++ master.R
@@ -96,7 +96,17 @@ saveGIF(play(rc=20,iter=50), interval=0.1)
 saveGIF(play(rc=10,seeds=c(1,1,1,3,2,2,2,3,3,2),iter=31), interval=0.2)
 
 ##Pulsar
-saveGIF(play(rc=20,seeds=c(),iter=12), interval=0.25)
+saveGIF(play(rc=15,seeds=c(2,4,2,5,2,6,2,10,2,11,2,12,
+                           7,4,7,5,7,6,7,10,7,11,7,12,
+                           9,4,9,5,9,6,9,10,9,11,9,12,
+                           14,4,14,5,14,6,14,10,14,11,14,12,
+                           4,2,4,7,4,9,4,14,
+                           5,2,5,7,5,9,5,14,
+                           6,2,6,7,6,9,6,14,
+                           10,2,10,7,10,9,10,14,
+                           11,2,11,7,11,9,11,14,
+                           12,2,12,7,12,9,12,14
+                           ),iter=12), interval=0.25)
 
 ##Big board (random seeds)
-saveGIF(play(rc=1000,iter=50), interval=0.1)
+saveGIF(play(rc=100,iter=50), interval=0.1)
